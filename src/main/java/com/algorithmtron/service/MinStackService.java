59423c1d5b66a3d60e105b894ec71e607400c969
Catalin Dobrescu <dobrescu.m.catalin@gmail.com>
2023-08-08 17:09:22 +0300

    Added MinStackService and Unit.

---
+++ src/main/java/com/algorithmtron/service/MinStackService.java
@@ -0,0 +1,72 @@
+package com.algorithmtron.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+@Service
+public class MinStackService {
+    /**
+     * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+     * Implement the MinStack class:
+     * MinStack() initializes the stack object.
+     * void push(int val) pushes the element val onto the stack.
+     * void pop() removes the element on the top of the stack.
+     * int top() gets the top element of the stack.
+     * int getMin() retrieves the minimum element in the stack.
+     *
+     */
+
+    Deque<Pair> stack;
+
+    private class Pair {
+        int val;
+        int min;
+
+        public Pair(int val, int min) {
+            this.val = val;
+            this.min = min;
+        }
+    }
+
+    public MinStackService() {
+        this.stack = new ArrayDeque<>();
+    }
+
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            stack.push(new Pair(val, val));
+        } else if (val < stack.peek().min) {
+            stack.push(new Pair(val,val));
+        } else {
+            stack.push(new Pair(val, stack.peek().min));
+        }
+    }
+
+    public boolean pop() {
+        if ( stack.size() == 0) {
+            return false;
+
+        } else {
+            stack.removeFirst();
+            return true;
+
+        }
+    }
+
+    public int top() {
+        if (stack.peek() == null) {
+            return Integer.MIN_VALUE;
+        }
+        return stack.peek().val;
+    }
+
+    public int getMin() {
+        if (stack.peek() == null) {
+            return Integer.MIN_VALUE;
+        }
+        return stack.peek().min;
+
+    }
+}
---
+++ src/test/java/com/algorithmtron/serviceTest/MinStackServiceTest.java
@@ -0,0 +1,92 @@
+package com.algorithmtron.serviceTest;
+
+import com.algorithmtron.service.MinStackService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class MinStackServiceTest {
+    @Test
+    void buildStackAndRetrieveTopElement () {
+        MinStackService minStackService = new MinStackService();
+
+        minStackService.push(3);
+        minStackService.push(2);
+
+        assertEquals(2,minStackService.top());
+    }
+
+    @Test
+    void buildStackAndGetMin () {
+        MinStackService minStackService = new MinStackService();
+
+        minStackService.push(3);
+        minStackService.push(1);
+        minStackService.push(2);
+
+        assertEquals(1,minStackService.getMin());
+    }
+
+    @Test
+    void getMinOnEmptyStack () {
+        MinStackService minStackService = new MinStackService();
+
+        Assertions.assertEquals(Integer.MIN_VALUE ,minStackService.getMin());
+    }
+
+    @Test
+    void retrieveTopOnEmptyStack () {
+        MinStackService minStackService = new MinStackService();
+
+        Assertions.assertEquals(Integer.MIN_VALUE ,minStackService.top());
+    }
+
+    @Test
+    void buildStackWithNegativeElemAndGetMin () {
+        MinStackService minStackService = new MinStackService();
+
+        minStackService.push(-3);
+        minStackService.push(23);
+
+        assertEquals(-3,minStackService.getMin());
+    }
+
+    @Test
+    void removingAnElementWhenTheStackIsEmpty () {
+        MinStackService minStackService = new MinStackService();
+
+        Assertions.assertFalse(minStackService.pop());
+    }
+
+    @Test
+    void removingAnElementWhenTheStackIsNotEmpty () {
+        MinStackService minStackService = new MinStackService();
+        minStackService.push(2);
+
+        Assertions.assertTrue(minStackService.pop());
+    }
+
+    @Test
+    void pushOneElementIntoStack () {
+        MinStackService minStackService = new MinStackService();
+        minStackService.push(2);
+
+        Assertions.assertEquals( 2, minStackService.top());
+        minStackService.pop();
+        Assertions.assertFalse(minStackService.pop());
+    }
+
+    @Test
+    void pushSecondElementIntoStackThatIsNewMinimum () {
+        MinStackService minStackService = new MinStackService();
+
+        minStackService.push(2);
+        minStackService.push(-1);
+
+        Assertions.assertEquals(-1, minStackService.getMin());
+    }
+}
